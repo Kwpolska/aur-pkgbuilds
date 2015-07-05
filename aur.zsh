5a9682ef10331dc4a3976088031116e7318fe8f6
Chris Warrick <kwpolska@gmail.com>
2015-07-05 21:04:50 +0200

    fix aur.zsh
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- aur.zsh
+++ aur.zsh
@@ -1,15 +1,13 @@
 mkaur() {
     pkg=$1
     ssh aur4.archlinux.org setup-repo $1
-    git clone ssh+git://aur4.archlinux.org/$1.git/
+    git submodule add ssh+git://aur4.archlinux.org/$1.git/
     cd $1
-    cp ~/git/aur-pkgbuilds/$1/* .
 }
 
 cpaur() {
     pkg=$1
     cd $1
-    cp ~/git/aur-pkgbuilds/$1/* .
 }
 
 commitaur() {
